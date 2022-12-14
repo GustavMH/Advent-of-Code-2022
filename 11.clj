6951a7bea3a890f5f889a5eb4d9c49d012590f4c
GustavMH <45123074+GustavMH@users.noreply.github.com>
2022-12-14 11:06:32 +0100

    Working solution to day 9 part 1

--- 11.clj
+++ 11.clj
@@ -11,15 +11,30 @@
         op (case op-str "+" + "*" *)]
     (op a b)))
 
-(operation 20 "old * old")
-
-(->> (slurp "input_11")
-    (#(str/split % #"\n\n"))
-    first
-    (re-find #"Monkey (\d+):
+(defn parse-monkey [s]
+  (let [[_1 _2 items-str op-str div-str
+         throw-true-str throw-false-str]
+        (re-find #"Monkey (\d+):
   Starting items: (.+)?
   Operation: new = (.+)?
   Test: divisible by (\d+)?
     If true: throw to monkey (\d+)
-    If false: throw to monkey (\d+)")
-    (drop 2))
+    If false: throw to monkey (\d+)" s)]
+    [(->> (str/split items-str #",")
+         (map (comp parse-int str/trim)))
+     op-str
+     (parse-int div-str)
+     (parse-int throw-true-str)
+     (parse-int throw-false-str)]))
+
+(def input
+  (->> (slurp "input_11")
+      (#(str/split % #"\n\n"))
+      (map parse-monkey)))
+
+(def rules (map rest input))
+(def items (map first input))
+(def handles (map (fn [& x] 0) input))
+
+(defn iter [[cur-rule & rules] items handles]
+  ())
--- 9.clj
+++ 9.clj
@@ -3,52 +3,32 @@
 (defn parse-int [s]
   (Integer/parseInt s))
 
-"R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2"
+(def moves {"L" [-1 0] "R" [1 0] "U" [0 1] "D" [0 -1]})
 
 (def steps
   (->> (slurp "input_9")
        str/split-lines
-       (map (fn [[d _ n]] [d (parse-int (str n))]))))
+       (map (partial re-find #"(.) (\d+)"))
+       (mapcat (fn [[_ d n]] (repeat (parse-int (str n)) d)))
+       (mapv moves)))
 
-(def moves {\L [-1 0] \R [1 0] \U [0 1] \D [0 -1]})
+(defn hi-diff [a b]
+  (->> (mapv (comp (partial < 1) abs -) a b)
+       (reduce #(or %1 %2))))
 
-(defn step-forward [pos dir]
-  (map + pos (get moves dir)))
+(defn move [[tails head] move]
+  (println tails head move)
+  (let [new-head (mapv + move head)]
+    (if (hi-diff (last tails) new-head)
+      [(conj tails head) new-head]
+      [tails new-head])))
 
-(defn step-back [pos dir]
-  (map - pos (get moves dir)))
+(->> (reduce move [[[0 0]] [0 0]] steps)
+     first (into #{}) count)
 
-(defn step-tail [[x y] [hx hy] dir]
-  (let [dx (abs (- hx x))
-        dy (abs (- hy y))]
-    (cond
-      (and (>= 1 dx)
-           (>= 1 dy)) [x y]
-      (or (>= 1 dx)
-          (>= 1 dy)) (step-back [hx hy] dir)
-      :else [x y])))
+(defn move-n [[tails knots] m]
+  (move [tails (first knots)] m))
+  ;(reduce #(move [%1 %2] m) tails knots))
 
-[(step-tail [0 0] [1 2] \U)
- (step-tail [0 0] [1 1] \U)
- (step-tail [0 0] [0 -2] \D)]
-
-(->> (do-steps steps [0 0] [0 0] #{}) count)
-
-(defn step [[h-pos t-pos ts] dir]
-  (let [new-h-pos (step-head h-pos dir)
-        new-t-pos (step-tail t-pos new-h-pos dir)]
-    ;(println h-pos "->" new-h-pos "," t-pos "->" new-t-pos "in" dir)
-    [new-h-pos new-t-pos (conj ts new-t-pos)]))
-
-(defn do-steps [[[dir n] & steps] h-pos t-pos ts]
-  (if (not steps) ts
-      (let [[new-h-pos new-t-pos new-ts]
-            (reduce step [h-pos t-pos ts] (repeat n dir))]
-        (do-steps steps new-h-pos new-t-pos new-ts))))
+(move-n [[[0 0]] (repeat 9 [0 0])] (first steps))
+(move-n [[0 0] [[0 0]]] (first steps))
